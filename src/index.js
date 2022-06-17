d5bba527a8b2771785f30a5f4bff9bdd939d6d84
Esther Bacelar <esther.bacelar@dex.company>
2022-06-17 18:50:45 -0300

    Changing basename

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Home from './pages/home/Home'
-import About from './pages/about/About'
-import Login from './pages/login/Login'
-import Results from './pages/login/sub-components/List-results';
-import RoutePrivate from './pages/login/sub-components/private';
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Login from "./pages/login/Login";
+import Results from "./pages/login/sub-components/List-results";
+import RoutePrivate from "./pages/login/sub-components/private";
 
-import './styles/index.css'
+import "./styles/index.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //Import Router//
 ReactDOM.render(
-  <BrowserRouter basename = "/searcher291121">
+  <BrowserRouter basename="/hashtagFinder291121">
     <Routes>
       <Route path="/" exact={true} element={<Home />} />
       <Route path="/sobre" element={<About />} />
       <Route path="/login" element={<Login />} />
-      <Route exact path='/results/*' element={<RoutePrivate />} >
-        <Route exact path="/results/*" element={<Results/>} />
+      <Route exact path="/results/*" element={<RoutePrivate />}>
+        <Route exact path="/results/*" element={<Results />} />
       </Route>
     </Routes>
-  </ BrowserRouter>
-  ,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById("root")
 );
