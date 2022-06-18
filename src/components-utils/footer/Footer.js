54e16d2a4f2c0a703f5f8a603fbd33509224bd2b
Esther Bacelar <esther.bacelar@dex.company>
2022-06-17 22:34:47 -0300

    Removing .&nbsp; and adding margin left to text footer

--- src/components-utils/footer/Footer.js
+++ src/components-utils/footer/Footer.js
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from "react";
 
-import styles from './Footer.module.css'
-
+import styles from "./Footer.module.css";
 
 const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <span>
+        @Newtab Academy {new Date().getFullYear()}. Todos os direitos reservados
+      </span>
+    </div>
+  );
+};
 
-    return ( 
-        <div className={styles.footer}>
-            <span>@Newtab Academy {new Date().getFullYear()}.&nbsp;&nbsp;Todos os direitos reservados</span>
-        </div>
-    )
-
-}
-
-
-export default Footer
+export default Footer;
